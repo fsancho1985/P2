78ecca8ec8fbb3b8f9f7ac3b8d2840db0e37a61c
Francisco Sancho <frsancho1985@gmail.com>
2025-06-25 09:04:44 -0300

    Adicionando conexão com Atlas

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
 
 export function connectDataBase() {
-    const URL_DB = "mongodb://localhost:27017/p2"
+    /**conexão com banco de dados local Compass */
+    // const URL_DB = "mongodb://localhost:27017/p2"
+
+    /**conexão com banco de dados remoto Atlas */
+    const URL_DB = "mongodb+srv://gustavo:gustavo@cluster0.egeh0mc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
     mongoose.connection.on("open", () => {
         console.log("Conectado ao mongoDB com sucesso!");
